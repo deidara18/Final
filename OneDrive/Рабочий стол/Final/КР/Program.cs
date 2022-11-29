6c8b26029ee3f3a62b057005810cb64a4af153fe
Maks <maks.kalinkin97@gmail.com>
2022-11-30 00:41:45 +0300

    Добавил решение задачи

---
+++ OneDrive/Рабочий стол/Final/КР/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string[] array1 = new string[5] {"dotnet", "123", "qwerty", "zxc", "Hello"};
+string[] array2 = new string[array1.Length];
+
+Sort(array1, array2);
+PrintArray(array1);
+Console.WriteLine("--------");
+PrintArray(array2);
+
+void Sort (string[] array1, string[] array2){
+    int temp = 0;
+    for(int i = 0; i < array1.Length; i++){
+        if (array1[i].Length <= 3){
+            array2[temp] = array1[i];
+            temp++; 
+        }
+    } 
+}
+
+void PrintArray(string[] Array){
+    for(int i = 0; i < Array.Length; i++){      
+            Console.Write($"{Array[i]} ");
+        Console.WriteLine();
+    }
+}
